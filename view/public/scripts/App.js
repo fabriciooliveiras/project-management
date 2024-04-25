d72e8cdc33dfb421f64aeb8352de651e4be32f50
Fabricio Oliveira <fabricio.oliveira.s@hotmail.com>
2024-04-25 14:12:01 -0300

    Page Navegation

--- view/public/scripts/App.js
+++ view/public/scripts/App.js
@@ -2,6 +2,8 @@ const App = function(){
     this.currentPage = 'Estimates';
     this.currentView = 'Tasks';
     this.pages = {};
+    this.listeners = [];
+    this.sideNav = 'CLOSE';
 }
 
 App.prototype.registerPage = function(Page){
@@ -10,22 +12,17 @@ App.prototype.registerPage = function(Page){
 
 App.prototype.render = function(container){
 
-    this.navContainer = Util.nodefy('<nav class="app-nav"><div class="app-nav-toggle-btn app-nav-padding"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title></title><g id="icomoon-ignore"></g><path d="M512 0v208l-80-80-96 96-48-48 96-96-80-80zM224 336l-96 96 80 80h-208v-208l80 80 96-96z"></path></svg></div><div class="app-nav-user-container app-nav-padding"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="448" height="448" viewBox="0 0 448 448"><title></title><g id="icomoon-ignore"></g><path d="M224 0c123.75 0 224 100.25 224 224 0 123-99.75 224-224 224-124 0-224-100.75-224-224 0-123.75 100.25-224 224-224zM378.75 337.75c23.25-32 37.25-71.25 37.25-113.75 0-105.75-86.25-192-192-192s-192 86.25-192 192c0 42.5 14 81.75 37.25 113.75 9-44.75 30.75-81.75 76.5-81.75 20.25 19.75 47.75 32 78.25 32s58-12.25 78.25-32c45.75 0 67.5 37 76.5 81.75zM320 176c0-53-43-96-96-96s-96 43-96 96 43 96 96 96 96-43 96-96z"></path></svg><span class="app-nav-text">Fabricio Oliveira</span></div><ul class="app-nav-menu-container"></ul></nav>');
+    this.navContainer = Util.nodefy('<nav class="app-nav"></nav>');
 
-    this.toggleBtn = this.navContainer.childNodes[0];
-    this.toggleBtn.onclick = ()=>{
+    //SIDE NAVEGATION ANIMATION
+    this.toggleBtn = Util.nodefy('<div class="app-nav-toggle-btn app-nav-padding"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title></title><g id="icomoon-ignore"></g><path d="M512 0v208l-80-80-96 96-48-48 96-96-80-80zM224 336l-96 96 80 80h-208v-208l80 80 96-96z"></path></svg></div>');
+    this.toggleBtn.onclick = this.toggleSideNav.bind(this);
 
-        let paddingList = document.querySelectorAll('.app-nav-padding');
-        for(let i=0;i<paddingList.length;i++){
-            paddingList[i].classList.toggle('app-nav-padding-none');
-        }
-        
-        let textList = document.querySelectorAll('.app-nav-text');
-        for(let i=0;i<textList.length;i++){
-            textList[i].classList.toggle('app-nav-text-none');
-        }
-    }
-
+    //USER INFORMATION
+    this.userInfoContainer = Util.nodefy('<div class="app-nav-user-container app-nav-padding"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="448" height="448" viewBox="0 0 448 448"><title></title><g id="icomoon-ignore"></g><path d="M224 0c123.75 0 224 100.25 224 224 0 123-99.75 224-224 224-124 0-224-100.75-224-224 0-123.75 100.25-224 224-224zM378.75 337.75c23.25-32 37.25-71.25 37.25-113.75 0-105.75-86.25-192-192-192s-192 86.25-192 192c0 42.5 14 81.75 37.25 113.75 9-44.75 30.75-81.75 76.5-81.75 20.25 19.75 47.75 32 78.25 32s58-12.25 78.25-32c45.75 0 67.5 37 76.5 81.75zM320 176c0-53-43-96-96-96s-96 43-96 96 43 96 96 96 96-43 96-96z"></path></svg><span class="app-nav-text">Fabricio Oliveira</span></div>');
+    
+    //PAGE NAVEGATION MENU
+    this.navMenuContainer = Util.nodefy('<ul class="app-nav-menu-container"></ul>');
     for(const key in this.pages){
         let menuIcon = Util.nodefy(`<li id="${key}" class="app-nav-icon-container  app-nav-padding"><span class="app-nav-icon">${this.pages[key].icon}</span><span class="app-nav-text">${this.pages[key].name}</span></li>`);
         
@@ -33,37 +30,68 @@ App.prototype.render = function(container){
 
         menuIcon.onclick = this.navBtnClick.bind(this,this.pages[key],menuIcon);
 
-        this.navContainer.childNodes[2].appendChild(menuIcon);
+        this.navMenuContainer.appendChild(menuIcon);
     }
     
+
     this.pagesContainer = Util.nodefy('<main class="app-body"></main>');
 
-
+    //DOM CREATON
+    this.navContainer.appendChild(this.toggleBtn);
+    this.navContainer.appendChild(this.userInfoContainer);
+    this.navContainer.appendChild(this.navMenuContainer);
     container.appendChild(this.navContainer);
+
     this.pagesContainer.appendChild(this.pages[this.currentPage].container)
     container.appendChild(this.pagesContainer);
+
+    //CHANGE NAV BAR ACORDING TO THE CURRENT VIEW
+    this.toggleSideNav();
 }
 
 App.prototype.navBtnClick = function(Page,menuIcon){
+    //UPDATE CURRENT PAGE
     this.currentPage = Page.name;
 
     for(const key in this.pages){
         if(key === this.currentPage){
 
+            //CHANGE BUTTON STYLE
             document.getElementById(key).classList.add('nav-bar-btn-clicked');
+
+            //LOAD BODY
             this.pagesContainer.innerHTML = '';
             this.pagesContainer.appendChild(this.pages[key].container);
 
         }else{
+
+            //CHANGE BUTTON STYLE
             document.getElementById(key).classList.remove('nav-bar-btn-clicked');
         }
     }
+}
+
+App.prototype.toggleSideNav = function(){
+
+     //PADDING ANIMATION
+     let paddingList = document.querySelectorAll('.app-nav-padding');
+     for(let i=0;i<paddingList.length;i++){
+         (this.sideNav === 'CLOSE') ? paddingList[i].classList.add('app-nav-padding-none') :  paddingList[i].classList.remove('app-nav-padding-none')
+     }
+     //TEXT ANIMATION
+     let textList = document.querySelectorAll('.app-nav-text');
+     for(let i=0;i<textList.length;i++){
+        (this.sideNav === 'CLOSE') ? textList[i].classList.add('app-nav-text-none') : textList[i].classList.remove('app-nav-text-none');
+     }
+
+     this.sideNav = (this.sideNav === 'OPEN') ? 'CLOSE' : 'OPEN';
 }
 
 //INIT
 const projectMenagement = new App();
 projectMenagement.registerPage(Estimates);
 projectMenagement.registerPage(Suport);
+
 
 //RENDER
 projectMenagement.render(document.getElementById('app'));
--- view/public/scripts/components/Page.js
+++ view/public/scripts/components/Page.js
@@ -2,21 +2,53 @@ const Page = function(obj){
     this.name = obj.name;
     this.icon = obj.icon;
     this.views = obj.views;
-    this.currentView = 0;
+    this.currentView = Object.keys(this.views)[0];
 }
 Page.prototype.render = function(){
     
     this.container = Util.nodefy('<div class="page-container"></div>');
-    this.menuContainer = Util.nodefy('<div class="page-menu-container"></div>');
+    this.menuContainer = Util.nodefy('<div class="page-view-container"></div>');
     this.bodyContainer = Util.nodefy('<div class="page-body-container"></div>');
 
-    for(let i=0;i<this.views.length;i++){
-        let tempItem = Util.nodefy(`<span class="page-menu-icon" title="${this.views[i].title}">${this.views[i].icon}</span>`)
+
+    //CREATES ALL THE VIEWS BUTTONS
+    for(const key in this.views){
+        let tempItem = Util.nodefy(`<span id="${key}" class="page-view-icon" title="${this.views[key].title}">${this.views[key].icon}</span>`)
+        
+        tempItem.onclick = this.viewBtnClick.bind(this,key);
+        
+        if(key == this.currentView) tempItem.classList.add('page-view-icon-cliked');
+
         this.menuContainer.appendChild(tempItem);
     }
 
-    this.bodyContainer.innerText = this.views[0].body;
+    //LOADS THE VIEW CONTENT
+    this.bodyContainer.innerText = this.views[this.currentView].body;
 
+    //DOM CREATION
     this.container.appendChild(this.menuContainer);
     this.container.appendChild(this.bodyContainer);
+}
+
+Page.prototype.viewBtnClick = function(view){
+
+    //UPDATES CURRENT VIEW
+    this.currentView = view;
+
+    let viewBtns = document.getElementsByClassName('page-view-container')[0].childNodes;
+    
+    //ANIMATE THE VIEWS BUTTONS
+    for (let i = 0; i < viewBtns.length; i++) {
+        const btn = viewBtns[i];
+        if(this.currentView === btn.getAttribute('id')){
+            btn.classList.add('page-view-icon-cliked');
+        }else{
+            btn.classList.remove('page-view-icon-cliked');
+        }
+    }
+
+    this.bodyContainer.innerHTML = '';
+    // this.bodyContainer.appendChild(this.views[this.currentView].body);
+    this.bodyContainer.innerHTML = this.views[this.currentView].body;
+
 }
